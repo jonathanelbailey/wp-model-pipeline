0884caf08acdf483f1cee004ce775f72804b5fa5
Jonathan Bailey <bailey.jonathan@hotmail.com>
2023-05-04 20:46:30 -0500

    updating dockerfile

--- components/train-model/build.sh
+++ components/train-model/build.sh
@@ -4,6 +4,6 @@ registry=$1
 image_name=$2
 tag=$3
 
-docker build --no-cache . -t $image_name:$tag
+docker build . -t $image_name:$tag
 docker image tag data-loader $registry/$image_name:$tag
 docker push $registry/$image_name:$tag
